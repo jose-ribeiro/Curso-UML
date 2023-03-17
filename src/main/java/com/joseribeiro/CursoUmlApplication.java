c39fa42ab7438b904db30a2b01b4cd4d8737f243
jose-ribeiro <57302604+jose-ribeiro@users.noreply.github.com>
2023-03-17 08:00:56 -0300

    Criando operacao de instanciacao

--- src/main/java/com/joseribeiro/CursoUmlApplication.java
+++ src/main/java/com/joseribeiro/CursoUmlApplication.java
@@ -1,13 +1,29 @@
 package com.joseribeiro;
 
+import com.joseribeiro.domain.Categoria;
+import com.joseribeiro.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoUmlApplication {
+import java.util.Arrays;
 
+@SpringBootApplication
+public class CursoUmlApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursoUmlApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+	}
 }
