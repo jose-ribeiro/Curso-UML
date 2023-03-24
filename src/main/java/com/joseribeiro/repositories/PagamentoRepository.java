e8ba8788f644185738219b87b0ca91fe1edd2587
jose-ribeiro <57302604+jose-ribeiro@users.noreply.github.com>
2023-03-24 16:15:59 -0300

    implementando as formas de pagamento

---
+++ src/main/java/com/joseribeiro/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.joseribeiro.repositories;
+
+
+import com.joseribeiro.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
---
+++ src/main/java/com/joseribeiro/repositories/PedidoRepository.java
@@ -0,0 +1,10 @@
+package com.joseribeiro.repositories;
+
+import com.joseribeiro.domain.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
